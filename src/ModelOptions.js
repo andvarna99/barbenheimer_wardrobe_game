a1e05ca54f6ca2b63e43094dfd4098d7d4204e56
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-09-05 18:32:34 -0500

    Create model options component

--- src/ModelOptions.js
+++ src/ModelOptions.js
@@ -1,13 +1,12 @@
 import React, {useState} from 'react';
 
 
-export default function ModelOptions({onBarbieModelBtnClick, onMurphyModelBtnClick}) {
-    const [activeModelOptionsBtn, setActiveModelOptionsBtn] = useState(null);
+export default function ModelOptions({onBarbieModelBtnClick, onMurphyModelBtnClick, onDownloadClick}) {
+    const [activeModelOptionsBtn, setActiveModelOptionsBtn] = useState("barbie-model-selector");
 
+    const [isDownloadHovered, setIsDownloadHovered] = useState(false);
     const [isBarbieProfileHovered, setIsBarbieProfileHovered] = useState(false);
-    // const [isBarbieProfileClicked, setIsBarbieProfileClicked] = useState(false);
     const [isMurphyProfileHovered, setIsMurphyProfileHovered] = useState(false);
-    // const [isMurphyProfileClicked, setIsMurphyProfileClicked] = useState(false);
 
     return (
         <div className="model-options">
@@ -36,9 +35,23 @@ export default function ModelOptions({onBarbieModelBtnClick, onMurphyModelBtnCli
                 <div className="murphy-profile "
                      style={{border: activeModelOptionsBtn === "murphy-model-selector" ? '2px solid #E9609B' : isMurphyProfileHovered ? "2px solid #E9609B" : 'none',}}></div>
                 <div className="murphy-profile-text col">
-                    <h5 className="murphy-header">Murphy</h5>
+                    <h5 className="murphy-header">Julius</h5>
                     <p className="murphy-header-text">This Ken's job is bomb...</p>
                 </div>
+            </button>
+            <button className={`download-btn rounded-pill ${isDownloadHovered ? "hover" : ""}`}
+                    type={"submit"}
+                    onMouseEnter={() => setIsDownloadHovered(true)}
+                    onMouseLeave={() => setIsDownloadHovered(false)}
+                    onClick={onDownloadClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     className="bi bi-download me-2" viewBox="0 0 16 16">
+                    <path
+                        d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                    <path
+                        d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                </svg>
+                Download
             </button>
         </div>
     )
